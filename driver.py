cedb6b656781781f602f75cc70183962290792d9
AdiModi96 <aditya.modi.in@gmail.com>
2020-04-15 21:07:44 +0530

    Version 1 Completed.

---
+++ driver.py
@@ -0,0 +1,18 @@
+from mail_companion import mail_composer, mailer
+
+mc = mail_composer()
+mc.set_receiver_email_id('email1@gmail.com')
+mc.add_receiver_cc_email_id(['cc_email1@gmail.com', '...'])
+mc.add_receiver_bcc_email_id(['bcc_email@gmail.com', '...'])
+mc.set_subject(subject='Test Mail')
+mc.set_message_body(
+    message_body='''
+    Hello,
+    # Enter Message Body Here.
+    ''', html=False)
+mc.add_attachments(['attachment1', 'attachment2', '...'])
+mc.compose_message()
+
+mmc = mailer()
+mmc.set_mail_composer(mc)
+mmc.send()
--- mail_companion.py
+++ mail_companion.py
@@ -10,7 +10,7 @@ import getpass
 credentials = {}
 
 
-class mail:
+class mail_composer:
     _sender_email_id = 'correspondent.automated@gmail.com'
 
     def __init__(self):
@@ -19,9 +19,8 @@ class mail:
         self._receiver_email_cc_ids = []
         self._receiver_email_bcc_ids = []
         self._subject = ''
-        self._body = ''''''
+        self._message_body = ''''''
         self._html = False
-        self._message_body = None
         self._mime_message = None
         self._attachments_file_paths = []
 
@@ -35,17 +34,31 @@ class mail:
         elif type(receiver_email_cc_ids) == str:
             self._receiver_email_cc_ids.append(receiver_email_cc_ids)
 
+    def add_receiver_bcc_email_id(self, receiver_email_bcc_ids):
+        if type(receiver_email_bcc_ids) == list:
+            for idx in range(len(receiver_email_bcc_ids)):
+                self._receiver_email_bcc_ids.append(receiver_email_bcc_ids[idx])
+        elif type(receiver_email_bcc_ids) == str:
+            self._receiver_email_bcc_ids.append(receiver_email_bcc_ids)
+
+    def add_attachments(self, attachment_file_paths):
+        if type(attachment_file_paths) == list:
+            for idx in range(len(attachment_file_paths)):
+                self._attachments_file_paths.append(attachment_file_paths[idx])
+        elif type(attachment_file_paths) == str:
+            self._attachments_file_paths.append(attachment_file_paths)
+
     def set_subject(self, subject):
         self._subject = subject
 
-    def set_body(self, body='''''', html=False):
-        self._body = body
+    def set_message_body(self, message_body='''''', html=False):
+        self._message_body = message_body
         self._html = html
 
     def compose_message(self):
         self._mime_message = MIMEMultipart('alternative')
         self._mime_message['Subject'] = self._subject
-        self._mime_message['From'] = mail._sender_email_id
+        self._mime_message['From'] = mail_composer._sender_email_id
         self._mime_message['To'] = self._receiver_email_id
         self._mime_message['Cc'] = str(self._receiver_email_cc_ids) \
             .replace('[', '') \
@@ -59,8 +72,14 @@ class mail:
             .replace('"', '')
         self._mime_message.attach(MIMEText(self._message_body, 'html' if self._html else 'plain'))
 
+        self._all_receiver_email_ids = [self._receiver_email_id]
+        for _receiver_email_cc_id in self._receiver_email_cc_ids:
+            self._all_receiver_email_ids.append(_receiver_email_cc_id)
+        for _receiver_email_bcc_id in self._receiver_email_bcc_ids:
+            self._all_receiver_email_ids.append(_receiver_email_bcc_id)
+
         for attachment_file_path in self._attachments_file_paths:
-            file_name = attachment_file_path[attachment_file_path.rindex(os.path.sep) + 1:]
+            file_name = os.path.basename(attachment_file_path)
             try:
                 with open(attachment_file_path, "rb") as attachment:
                     part = MIMEBase("application", "octet-stream")
@@ -74,29 +93,45 @@ class mail:
                 "attachment; filename={}".format(file_name),
             )
             self._mime_message.attach(part)
+        print('Mail composed successfully!')
 
 
 class mailer:
 
-    def __init__(self, mail_object):
-        self.mail_object = mail_object
-        self.password = ''
+    def __init__(self):
+        self._mail_composer = None
+        self._password = ''
         self.context = ssl.create_default_context()
 
         try:
             for i in range(3):
-                password = getpass.getpass(prompt='Enter your Gmail Password: ')
+                password = getpass.getpass(prompt='Enter your password for account "{}": '.format(mail_composer._sender_email_id))
                 try:
                     with smtplib.SMTP_SSL("smtp.gmail.com", context=self.context) as server:
-                        server.login(mail._sender_email_id, password)
-                        self.password = password
+                        server.login(mail_composer._sender_email_id, password)
+                        self._password = password
                         break
                 except smtplib.SMTPAuthenticationError:
                     print('Error: Authentication Failed!')
                     print('{} attempts left'.format(i - 1))
 
-            with smtplib.SMTP_SSL("smtp.gmail.com", context=self.context) as server:
-                server.login(mail._sender_email_id, self.password)
-                server.sendmail(mail._sender_email_id, mail_object._receiver_email_id, mail_object._mime_message.as_String())
         except:
             print('Error: Unexpected Error!')
+
+    def set_mail_composer(self, mail_composer):
+        self._mail_composer = mail_composer
+
+    def send(self):
+        if mail_composer != None:
+            try:
+                with smtplib.SMTP_SSL("smtp.gmail.com", context=self.context) as server:
+                    server.login(mail_composer._sender_email_id, self._password)
+                    server.sendmail(mail_composer._sender_email_id,
+                                    self._mail_composer._all_receiver_email_ids,
+                                    self._mail_composer._mime_message.as_string())
+                print('Mail sent successfully!')
+            except:
+                print('Error: Unexpected Error!')
+        else:
+            print('Error: Compose a mail first!')
+
